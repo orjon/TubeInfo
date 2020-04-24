18652e4aba50055652a344ade9899f78811d8a5a
orjon <github@orjon.com>
2020-04-24 16:52:32 +0100

    Station names trimmed

--- src/LineStop.js
+++ src/LineStop.js
@@ -3,12 +3,27 @@ import './LineStop.scss';
 
 class LineStop extends Component{
   render(){
-    let stopName = this.props.stop.stopName
+    let stop = this.props.stop
+
+    let stopName = ''
+      if (stop.stopName) {
+        stopName = stop.stopName
+        stopName = stopName.replace('Underground Station', '')
+        let cropIndex1 = stopName.indexOf('(') + 1
+        console.log('cropIndex:', cropIndex1)
+        if (cropIndex1 != 0){
+          console.log('slicing')
+          stopName = stopName.slice(0, (cropIndex1-2))
+        }
+        
+      }
+
     // if (stopName){
+
     //     let cropIndex1 = stopName.indexOf(':') + 1
-    //     if (cropIndex1 != -1){
-    //       stopName = stopName.slice(0, cropIndex1)
-    //     }
+        // if (cropIndex1 != -1){
+        //   stopName = stopName.slice(0, cropIndex1)
+        // }
     //     let cropIndex2 = stopName.indexOf('(') + 1
     //     if (cropIndex1 != -1){
     //       stopName = stopName.slice(0, cropIndex2)
@@ -16,9 +31,7 @@ class LineStop extends Component{
     //   }
     
     return(
-      <div className='LineStop'>
-        {stopName}
-      </div>
+      <div className='LineStop'>{stopName}</div>
     )
   }
 }
